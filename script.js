303de1c32021af7cc3857b8db0367f00abb80e22
William Reima <william.reima@gmail.com>
2022-01-11 21:14:08 +0200

    Added WorldTimeAPI

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("testirtrt");
+console.log("testi");
+
+const tableBody = document.getElementById("h2");
+
+const MyAsyncFunction = async () => {
+  console.log("Entering asyncfunction");
+
+  const response = await fetch("http://worldtimeapi.org/api/ip");
+
+  const Data = await response.json();
+  console.log(Data);
+};
+MyAsyncFunction();
